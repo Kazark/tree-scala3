d0e678c30d77f404c735f41860fec5bf74ab1f2b
Keith Pinson <keith.pinson@banno.com>
2020-07-24 14:54:13 -0400

    refactor: try out @main and braceless blocks

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,5 @@
+@main def main(): Unit =
+  println("Hello world!")
+  println(msg)
 
-object Main {
-
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println(msg)
-  }
-
-  def msg = "I was compiled by dotty :)"
-
-}
+def msg = "I was compiled by dotty :)"
