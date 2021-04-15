2ab3f7735257d4bccd8257a9739095924856ed70
Keith Pinson <keith.pinson@banno.com>
2021-04-15 09:59:17 -0400

    get onto latest Scala 3 and add Cats

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ lazy val root = project
     name := "dotty-simple",
     version := "0.1.0",
     testFrameworks += new TestFramework("munit.Framework"),
-    scalaVersion := "3.0.0-RC1",
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.22" % Test
+    scalaVersion := "3.0.0-RC2",
+    libraryDependencies ++= Seq(
+      "org.scalameta" %% "munit" % "0.7.23" % Test,
+      "org.typelevel" %% "cats-core" % "2.5.0",
+    )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,4 @@
-package calc
-
-enum Expr:
-  case EInt(value: Int)
-  case Add(leftAddend: Expr, rightAddend: Expr)
-  case Minus(minuend: Expr, subtrahend: Expr)
-
-def eval(expr: Expr): Int =
-  expr match
-    case Expr.Add(l, r) => eval(l) + eval(r)
-    case Expr.Minus(m, s) => eval(m) - eval(s)
-    case Expr.EInt(value) => value
+package metalepsis
 
 @main def main(): Unit =
-  val result = eval(Expr.Minus(minuend = Expr.EInt(256), subtrahend = Expr.Add(Expr.EInt(128), Expr.EInt(64))))
-  println(result)
+  println("Hello, world!")
